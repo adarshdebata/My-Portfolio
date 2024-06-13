720a1caf7badda4ff24eae80a0ff92558b29ff19
adarshdebata <adarshdebata00@gmail.com>
2024-06-13 16:59:28 +0530

    script.js Update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// scrolling Switch
+//menu
+let menuIcon = document.querySelector('#menuIcon');
+let navBar = document.querySelector('nav');
+
+menuIcon.oclick = () => {
+    menuIcon.classList.toggle('bx-x');
+}
+// scrolling 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    // sticky Header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
